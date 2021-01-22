77de7b29fb23ca585c289b0c101e1d92700f675d
Mark Yoder <mark.yoder@gmail.com>
2021-01-22 10:38:22 -0800

    more mods to AM4py.py and smaller scripts. adding some sampel run-scripts; adding additional input files to facilitate empty(ish) work_dir runs.

--- am4_sample_run.py
+++ am4_sample_run.py
@@ -5,23 +5,25 @@ import subprocess
 
 if __name__ == '__main__':
     #
-    input_data_path = os.path.join(os.environ['HOME'], 'Codes', 'AM4_data2', 'AM4_run')
-    work_dir = os.path.join(os.environ['HOME'], 'Codes', 'AM4_runtime', 'workdir2')
+    input_data_path = os.path.join(os.environ['SCRATCH'], 'AM4', 'AM4_data3', 'AM4_run')
+    work_dir = os.path.join(os.environ['SCRATCH'], 'AM4', 'workdir3')
     job_name = 'AM4_dev'
     batch_job_name = os.path.join(work_dir, 'AM4_batch_example.bs')
+    n_cpu_atmos = 48
     #
     #print('*** input_data_path: ', input_data_path)
     #
-    ABS = AM4py.AM4_batch_scripter(input_data_path=imput_data_path, work_dir=work_dir,
-                               job_name='AM4_dev', batch_out=batch_job_name )
+    ABS = AM4py.AM4_batch_scripter(input_data_path=input_data_path, work_dir=work_dir, npes_atmos=n_cpu_atmos,
+                               job_name='AM4_dev', batch_out=batch_job_name,
+                                verbse=True )
     #
     print('*** ABS variables:')
     for key,val in ABS.__dict__.items():
-    print('{}: {}'.format(key,val))
+        print('{}: {}'.format(key,val))
     #
     zz = ABS.get_input_data(verbose=True)
     #
-    my_configs = {'coupler_nml':{'days':10, 'months':0}, 'fv_core_nml':{'npx':193, 'npy':193, 'npz':50}}
+    my_configs = {'coupler_nml':{'days':15, 'months':0}, 'fv_core_nml':{'npx':97, 'npy':97, 'npz':33}}
     my_nml = ABS.make_NML(nml_template='input_yoder_v101.nml', nml_configs=[my_configs],
                       nml_out=os.path.join(ABS.work_dir, 'input.nml') )
     # NML_from_nml('input_yoder_v101.nml')
@@ -35,4 +37,4 @@ if __name__ == '__main__':
     #
     ABS.write_batch_script()
     #
-    #sbatch_output = subprocess.run('sbatch {}'.format(ABS.batch_out), check=True, capture_output=True )
+    sbatch_output = subprocess.run('sbatch {}'.format(ABS.batch_out), shell=True, check=True, capture_output=True )
